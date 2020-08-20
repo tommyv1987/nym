fbdb8f9c2f10bbfff1debc34b7ece5d4819384f2
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2020-08-20 13:17:55 +0100

    Initial packetizer structure

---
+++ common/client-libs/tcp-packetizer/src/lib.rs
@@ -0,0 +1,10 @@
+#[cfg(test)]
+mod test_packetizing_and_reassembling {
+    use super::*;
+
+    #[test]
+    fn test_packetizing_bytes_produces_sphinx_packet() {}
+
+    #[test]
+    fn test_reassembling_sphinx_packets_using_sequence_number_produces_original_bytes() {}
+}
